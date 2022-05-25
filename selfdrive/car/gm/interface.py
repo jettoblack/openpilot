c8e1a6a4c3f6be537afc8282f20bba2a7cfe091e
Tim Wilson <twilsonco@gmail.com>
2022-05-25 15:02:10 -0600

    Acadia long lower mod-speed ki kp

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -210,10 +210,10 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.pid.kdV = [0.6]
         ret.lateralTuning.pid.kf = 1. # get_steer_feedforward_acadia()
 
-      ret.longitudinalTuning.kpBP = [5., 15., 35.]
-      ret.longitudinalTuning.kpV = [1.25, 1.6, 1.3]
-      ret.longitudinalTuning.kiBP = [5., 15., 35.]
-      ret.longitudinalTuning.kiV = [0.18, 0.28, 0.30]
+      ret.longitudinalTuning.kpBP = [5., 20., 35.]
+      ret.longitudinalTuning.kpV = [1.25, 1.4, 1.3]
+      ret.longitudinalTuning.kiBP = [5., 20., 35.]
+      ret.longitudinalTuning.kiV = [0.18, 0.25, 0.30]
       ret.longitudinalTuning.kdBP = [5., 25.]
       ret.longitudinalTuning.kdV = [0.6, 0.0]
 
