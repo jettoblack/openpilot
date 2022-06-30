500be994a0dc91b057b81781667f65910d6f0096
Tim Wilson <twilsonco@gmail.com>
2022-06-30 16:13:05 -0600

    apply 2% grade deadzone to pitch-corrected long control

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -125,6 +125,7 @@ class CarState(CarStateBase):
     self.pitch_raw = 0.
     self.pitch_ema = 1/200
     self.pitch_accel_factor = 0.8
+    self.pitch_accel_deadzone = 0.02 # radians ~ ±2% grade
     
     # similar to over-speed coast braking, lockout coast/one-pedal logic first for engine/regen braking, and then for actual brakes.
     # gas lockout lookup tables:
