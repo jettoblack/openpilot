50288fd27e67eeddf20aca543aec8448f451587d
Tim Wilson <twilsonco@gmail.com>
2022-12-03 18:45:08 -0700

    add lat_pid curvature rate-based FF term per hewers

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -1,6 +1,7 @@
 import math
 
 from cereal import log
+from common.conversions import Conversions as CV
 from selfdrive.controls.lib.latcontrol import LatControl, MIN_STEER_SPEED
 from selfdrive.controls.lib.pid import PIDController
 
@@ -36,7 +37,15 @@ class LatControlPID(LatControl):
     else:
       # offset does not contribute to resistive torque
       steer_feedforward = self.get_steer_feedforward(angle_steers_des_no_offset, CS.vEgo)
-
+      
+      # torque for steer rate. ~0 angle, steer rate ~= steer command.
+      steer_rate_actual = CS.steeringRateDeg
+      steer_rate_desired = math.degrees(VM.get_steer_from_curvature(-desired_curvature_rate, CS.vEgo, 0))
+      speed_mph =  CS.vEgo * CV.MS_TO_MPH
+      steer_rate_max = 0.0389837 * speed_mph**2 - 5.34858 * speed_mph + 223.831
+      steer_feedforward += ((steer_rate_desired - steer_rate_actual) / steer_rate_max)
+      
+      
       output_steer = self.pid.update(error, override=CS.steeringPressed,
                                      feedforward=steer_feedforward, speed=CS.vEgo)
       pid_log.active = True
